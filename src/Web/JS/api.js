5bf1dffe14bfc0b21dbd0e232b5e063f4f3dfa1d
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2024-06-17 13:44:39 +0200

    ajout de modification html et js pour l'api meteo

--- src/Web/JS/api.js
+++ src/Web/JS/api.js
@@ -8,6 +8,18 @@ export async function velibs_status(){
     return await response.json();
 }
 
+export async function dataMeteo() {
+    try {
+        const response = await fetch('https://www.infoclimat.fr/public-api/gfs/json?_ll=48.687,6.222&_auth=BB5TRFEvACJRfAE2AnRXflE5V2IBd1J1BnoCYQlsUi9SN1U5BmJQLVEhBGBTfVJkU2JQMgoqAiUBYlIyXTFTOARnUyhRLQB%2FUT8BfAItV2JRa1c0ASBSbwZlAnsJZVIyUjRVLwZnUDJROAR5U3xSZ1NnUDEKPQI4AWJSNl0wUzgEZFMoUS0AZFE4AWUCO1cxUWBXZAFsUj8GYwIwCWVSOVI5VS8GYFAzUTkEYFNqUm9TZFA1CioCJQEbUkZdLVNwBCVTYlF0AH9RawE9AmY%3D&_c=d9e5c2412377972a721b3086672c43ca');
+        if (!response.ok) {
+            throw new Error(`Erreur: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Api Non Chargé", error);
+    }
+}
 
 /*
 idées:
