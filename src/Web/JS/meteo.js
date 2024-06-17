473a09148646dfb38eb710288dd5412a4e1c9077
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2024-06-17 14:22:37 +0200

    ajout temperature

--- src/Web/JS/meteo.js
+++ src/Web/JS/meteo.js
@@ -1,21 +1,23 @@
+import { dataMeteo } from './api.js';
+
 const API_HOURS = [" 02:00:00"," 05:00:00"," 08:00:00"," 11:00:00"," 14:00:00"," 17:00:00"," 20:00:00"," 23:00:00"," 23:00:00"];
 var API_TEMP = [];
 var API_PLUIE = [];
 
 function getCurrentDate() {
-    currentDate = new Date();
-    year = currentDate.getFullYear();
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
     if (currentDate.getMonth() < 9) {
-        month = "0" + (currentDate.getMonth() + 1).toString();
+        var month = "0" + (currentDate.getMonth() + 1).toString();
     }else{
-        month = currentDate.getMonth().toString();
+        var month = currentDate.getMonth().toString();
     }
     const day = String(currentDate.getDate());
     return `${year}-${month}-${day}`;
 }
 
 async function getTodayPrevision(){
-    data = await loadApi();
+    let data = await dataMeteo();
     const currentDate = getCurrentDate();
     let i = 0;
     API_HOURS.forEach(element => {
@@ -29,31 +31,33 @@ async function getTodayPrevision(){
 }
 
 function calcTemp() {
-    tMatin = (API_TEMP[1] + API_TEMP[2] + API_TEMP[3]) / 3;
-    tAprem = (API_TEMP[3] + API_TEMP[4] + API_TEMP[5]) / 3;
-    tSoir = (API_TEMP[5] + API_TEMP[6] + API_TEMP[7]) / 3;
-    console.log(Math.round(tMatin*100)/100 + " C");
-    console.log(Math.round(tAprem*100)/100 + " C");
-    console.log(Math.round(tSoir*100)/100 + " C");
-
+    let tMatin = (API_TEMP[1] + API_TEMP[2] + API_TEMP[3]) / 3;
+    let tAprem = (API_TEMP[3] + API_TEMP[4] + API_TEMP[5]) / 3;
+    let tSoir = (API_TEMP[5] + API_TEMP[6] + API_TEMP[7]) / 3;
+    let texteMatin = document.getElementById("txt_matin");
+    texteMatin.innerHTML = Math.round(tMatin*100)/100 + " C";
+    let texteAprem = document.getElementById("txt_aprem");
+    texteAprem.innerHTML = Math.round(tAprem*100)/100 + " C";
+    let texteSoir = document.getElementById("txt_soir");
+    texteSoir.innerHTML = Math.round(tSoir*100)/100 + " C";
 }
 
 function calcPluie() {
-    pMatin = (API_PLUIE[1] + API_PLUIE[2] + API_PLUIE[3]) / 3;
-    pAprem = (API_PLUIE[3] + API_PLUIE[4] + API_PLUIE[5]) / 3;
-    pSoir = (API_PLUIE[5] + API_PLUIE[6] + API_PLUIE[7]) / 3;
+    let pMatin = (API_PLUIE[1] + API_PLUIE[2] + API_PLUIE[3]) / 3;
+    let pAprem = (API_PLUIE[3] + API_PLUIE[4] + API_PLUIE[5]) / 3;
+    let pSoir = (API_PLUIE[5] + API_PLUIE[6] + API_PLUIE[7]) / 3;
     tstPluie(pMatin);
     tstPluie(pAprem);
     tstPluie(pSoir);
 }
 
-function tstPluie(nP){
+function tstPluie(nP,element){
     if (nP > 0.2) {
-        console.log("Il va pleuvoir");
+        element.src = "../img/rain.png";
     } else if (nP > 0) {
-        console.log("Il va y avoir des nuages");
+        element.src = "../img/cloud.png";
     } else {
-        console.log("Il va faire beau");
+        element.src = "../img/sun.png";
     }
 }
 
