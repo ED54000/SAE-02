7a4a7d32ebfddb588894d319d7461d7d692b08cd
Fiori4444 <florian.bortolotti88@gmail.com>
2024-06-17 16:52:52 +0200

    modif reservation

--- src/JDBC/RequetesSql.java
+++ src/JDBC/RequetesSql.java
@@ -49,15 +49,39 @@ public class RequetesSql {
     public boolean addReserv(Map<String, String> map) {
         try {
             Connection connection = ConnectionDb.getConnection();
-            String sql = "INSERT INTO RESERV (IDRESTAURANT,NOM,PRENOM,NBCONVIVES,COORD_TEL) " +
+            String sql_insert = "INSERT INTO RESERV (IDRESTAURANT,NOM,PRENOM,NBCONVIVES,COORD_TEL) " +
                          "VALUES ("+ map.get("idrestaurant") +", '"+ map.get("nom") +"', '"+ map.get("prenom") +"', "+ map.get("nbconvives") +", '"+ map.get("tel") +"')";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.execute();
-            System.out.println("Reservation ajouté avec succès");
-            return true;
+            PreparedStatement preparedStatement1 = connection.prepareStatement(sql_insert);
+            preparedStatement1.execute();
+            int newNbPlaces = getNbPlaces(Integer.parseInt(map.get("idrestaurant"))) - Integer.parseInt(map.get("nbconvives"));
+            if(newNbPlaces < 0) {
+                System.out.println("Pas assez de places disponibles");
+                return false;
+            } else {
+                String sql_update = "UPDATE RESTAU SET (NBPLACES) = "+ newNbPlaces +" WHERE ID = "+ map.get("idrestaurant");
+                PreparedStatement preparedStatement2 = connection.prepareStatement(sql_update);
+                preparedStatement2.execute();
+                System.out.println("Reservation ajouté avec succès");
+                return true;
+            }
         } catch (SQLException e) {
             System.out.println("erreur lors l'ajout de la reservation : " + e.getMessage());
         }
         return false;
+    }
+
+    public int getNbPlaces(int id) {
+        try {
+            Connection connection = ConnectionDb.getConnection();
+            String sql = "SELECT NBPLACES FROM RESTAU WHERE ID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet set = preparedStatement.executeQuery();
+            set.next();
+            return set.getInt(1);
+        } catch (SQLException e) {
+            System.out.println("erreur lors la connexion : " + e.getMessage());
+        }
+        return -1;
     }
 }
